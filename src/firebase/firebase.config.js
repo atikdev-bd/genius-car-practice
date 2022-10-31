82b14660d6d625e8cba6bacf0c415725225da18d
atikdev-bd <atikdev.bd@gmail.com>
2022-10-31 16:34:46 +0600

    genius car practice day

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbizmT3dLvJmi_CeDC4Mq9P8ddribAf14",
+  authDomain: "genius-car-firebase-f6281.firebaseapp.com",
+  projectId: "genius-car-firebase-f6281",
+  storageBucket: "genius-car-firebase-f6281.appspot.com",
+  messagingSenderId: "875168810653",
+  appId: "1:875168810653:web:93d306ac7c9a9d03873674",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
